c6293adaa90a2fcf66b0620d11b90012cb43d48c
ieb-jk <john.kemp@allbeauty.com>
2018-07-24 01:29:47 +0100

    Testing pipes

--- src/com/ieb/pipeline/Builder.groovy
+++ src/com/ieb/pipeline/Builder.groovy
@@ -1,5 +1,57 @@
 package com.ieb.pipeline;
 
 void build() {
-    echo "Testing library"
+	
+    agent { label 'Worker1' }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                echo 'Checkout source, report conflicts and issues with migrations or grunt'
+            }
+        }
+
+        stage('UnitTesting') {
+            steps {
+                echo 'PhpUnit - component testing with mock api stubs'
+                sh "composer install"
+                sh "./vendor/bin/phpunit --log-junit reports/phpunit.xml --coverage-clover reports/coverage.xml --coverage-html reports/coverage"
+            }
+        }
+
+        stage('ParallelTesting') {
+            steps {
+                parallel (
+                    "StaticAnalysis" : { echo 'SonarPHP - Codesniffer, LinesOfCode, MessDetector, CopyPaste Detector, CodeBrowser, DOX' },
+                    "Integration" : { echo 'BrowserStack with end to end testing' },
+                    "LoadTesting" : { echo 'JMeter, Bench, Seige' },
+                    "Security" : { echo 'RIPs security scanning' }
+                )
+            }
+        }
+
+        stage('ReleaseToDark') {
+            when {
+                branch "PR-..*"
+            }
+            steps {
+                echo "Deploy to 'dark' environment"
+            }
+        }
+    }
+
+    post {
+        always {
+            junit 'reports/*.xml'
+            publishHTML target: [
+                allowMissing: false,
+                alwaysLinkToLastBuild: false,
+                keepAll: true,
+                reportDir: 'reports/coverage',
+                reportFiles: 'index.html',
+                reportName: 'Coverage Report'
+            ]
+        }
+    }
 }
