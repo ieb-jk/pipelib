c9cd438e23f923b23d256b5d722222abb8b073a6
ieb-jk <john.kemp@allbeauty.com>
2018-07-29 12:58:09 +0100

    Adding more stages

--- src/com/ieb/pipeline/Builder.groovy
+++ src/com/ieb/pipeline/Builder.groovy
@@ -5,4 +5,10 @@ void build() {
 	stage("Build Application") {
 		echo "We are again in the library"
 	}
+	
+	stage("UnitTesting") {
+		echo 'PhpUnit - component testing with mock api stubs'
+		sh "echo 'running composer install'"
+		sh "echo 'now we would run ./vendor/bin/phpunit --log-junit reports/phpunit.xml --coverage-clover reports/coverage.xml --coverage-html reports/coverage'"
+	}
 }
