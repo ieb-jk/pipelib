ad951e0629030a58f63d775df11c76eb93d7a298
ieb-jk <john.kemp@allbeauty.com>
2018-07-29 13:07:24 +0100

    With parallel

--- src/com/ieb/pipeline/Builder.groovy
+++ src/com/ieb/pipeline/Builder.groovy
@@ -11,4 +11,13 @@ void build() {
 		sh "echo 'running composer install'"
 		sh "echo 'now we would run ./vendor/bin/phpunit --log-junit reports/phpunit.xml --coverage-clover reports/coverage.xml --coverage-html reports/coverage'"
 	}
+	
+	stage('ParallelTesting') {
+		parallel (
+			"StaticAnalysis" : { echo 'SonarPHP - Codesniffer, LinesOfCode, MessDetector, CopyPaste Detector, CodeBrowser, DOX' },
+			"Integration" : { echo 'BrowserStack with end to end testing' },
+			"LoadTesting" : { echo 'JMeter, Bench, Seige' },
+			"Security" : { echo 'RIPs security scanning' }
+		)
+	}
 }
